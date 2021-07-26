81fadee740c5ec23c7ee60c3e3f9f2de2b9eea62
Vesal <vmvvahdatpour@yahoo.com>
2021-07-25 20:15:01 -0600

    updated main

--- v2/main.py
+++ v2/main.py
@@ -2,37 +2,33 @@ import os
 from converter import convert_to_client_data
 from reader import Reader
 
+from datetime import datetime
+start_time = datetime.now()
+
 # data_type = 'csv'
-# input_path = './data/csv/fake_job_postings.csv'  # accepts either folder or csv file
+# input_path = '/Users/Vesal/Desktop/federated-learning-tools/v2/data/csv/fake_job_postings_newLabel.csv'  # accepts only a csv file
 
-data_type = 'text'
-input_path = './data/text/topics_sample'  # accepts either folder or csv file
+# data_type = 'text'
+# input_path = '/Users/Vesal/Desktop/federated-learning-tools/v2/data/text/topics_sample'  # accepts either folder or csv file
 
-# data_type = 'image'
-# input_path = './data/image/fish_sample'  # accepts either folder or csv file
+data_type = 'image'
+input_path = '/Users/Vesal/Desktop/federated-learning-tools/v2/data/image/fish_sample'  # accepts folder only
 
 # Read Data
 obj = Reader(data_type, input_path)
 data, labels = obj.read_data()
 
 # Distribute Data and return clients
+NUM_CLIENTS = 10
+SELECTED_FEATURE = 17
 dataset = convert_to_client_data(data, labels,  data_type,
-                                                type = 'niid',
-                                                number_of_clients = 20,
-                                                selected_feature=3,
+                                                dist_type = 'niid',
+                                                number_of_clients = NUM_CLIENTS,
+                                                selected_feature=SELECTED_FEATURE,
                                                 min_seen_labels=2,
-                                                max_seen_labels=4)
+                                                max_seen_labels=4
+                                                )
 
+end_time = datetime.now()
 
-print('Finished loading client data!')
-
-print(dataset.client_ids[-1])
-
-# example_dataset = dataset.create_tf_dataset_for_client(dataset.client_ids[-1])
-# example_element = next(iter(example_dataset))
-# #
-# #
-# print(example_element['label'].numpy())
-# print(example_element['data'].numpy())
-#
-# print(len(example_element['data'].numpy()))
+print("Finished loading client data! Total run time:", (end_time - start_time))
